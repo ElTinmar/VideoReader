b279222580d5b6014f48eebc1e2dc60aa3257e2a
ElTinMar <martin.privat@hotmail.fr>
2022-07-15 12:10:55 +0200

    parse command line

--- producer_consumer.py
+++ producer_consumer.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 from multiprocessing import Process, Queue, JoinableQueue
 import cv2
 import time
 import os
 import queue
-import numpy
+import numpy as np
 import argparse
 
 def producer(path, use_gpu, frame_queue):
@@ -43,7 +45,8 @@ def consumer(frame_queue, result_queue, process_num, process_fun):
         except queue.Empty:
             pass
         
-def start(frame_queue, result_queue, videofile, n_consumers=1, use_gpu=False, process_fun=process):
+def start(frame_queue, result_queue, videofile, n_consumers,
+            process_fun, use_gpu):
     """spawn all the processes"""
 
     # start consumers and producers                                             
@@ -51,7 +54,7 @@ def start(frame_queue, result_queue, videofile, n_consumers=1, use_gpu=False, pr
     for i in range(n_consumers):                                                
         p = Process(
             target=consumer, 
-            args=(frame_queue, result_queue, i, process,),
+            args=(frame_queue, result_queue, i, process_fun,),
             daemon=True) # allow consumers to be closed when queue is empty
         consumer_process.append(p)                                              
         p.start()
@@ -60,8 +63,12 @@ def start(frame_queue, result_queue, videofile, n_consumers=1, use_gpu=False, pr
                         args=(videofile,use_gpu,frame_queue,))    
     producer_process.start()
 
-    # wait for producers to terminate                             
-    producer_process.join()
+    # wait for producers to terminate
+    try:
+        producer_process.join()
+    except KeyboardInterrupt:
+        producer_process.terminate()
+        producer_process.join()
    
 def busy_wait(dt):   
     current_time = time.time()
@@ -88,17 +95,67 @@ def process(frame,frame_num,frame_queue,result_queue):
 
 if __name__ == "__main__":
     
-    # set parameters
-    frame_queue = JoinableQueue(maxsize = 2048)
+    parser = argparse.ArgumentParser(
+        description='Benchmark producer-consumer video reader'
+    )
+    parser.add_argument(
+        'videofile', 
+        type = str,
+        help = 'Path to the video'
+    )
+    parser.add_argument(
+        '-n',
+        type = int,
+        default = 1,
+        help = 'number of consumer processes'
+    )
+    parser.add_argument(
+        '--queuesize',
+        '-q',
+        default = 2048,
+        type = int,
+        help = 'Max size of the frame buffer'
+    )
+    parser.add_argument(
+        '--load',
+        type = str,
+        default = "MC",
+        help = 'Synthetic load: light L, single core SC, multicore MC'
+    )
+    parser.add_argument(
+        '--gpu',
+        action = 'store_true',
+        help = 'Use GPU for hardware accelerated decoding with FFMPEG'
+    )
+    args = parser.parse_args()
+
+    # check that video file exists
+    videofile = args.videofile
+    if not os.path.exists(videofile):
+         raise FileNotFoundError
+
+    qsize = args.queuesize
+    n_consumers = args.n
+
+    frame_queue = JoinableQueue(maxsize = qsize)
     result_queue = Queue()
-    videofile = '/home/martin/jumanji.mp4'
-    n_consumers = 1
+    n_consumers = args.n
+    gpu = args.gpu
+    
+    if (args.load == "MC"):
+        pfun = synthetic_load_multi_core
+    elif (args.load == "SC"):
+        pfun = synthetic_load_single_core
+    elif (args.load == "L"):                                                
+        pfun = synthetic_load_light
+    else:
+        raise ValueError
 
     start(
         frame_queue, 
         result_queue, 
         videofile, 
         n_consumers, 
-        use_gpu=True, 
-        process_fun=synthetic_load_multi_core
+        pfun,
+        gpu
     )
