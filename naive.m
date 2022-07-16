009fd5bd0d657cb83e130fe69906a1e02e2ff142
ElTinMar <martin.privat@hotmail.fr>
2022-07-16 12:30:17 +0200

    fix error

--- naive.m
+++ naive.m
@@ -1,19 +1,20 @@
 function naive(videofile)
 
     function process(frame, frame_num)
-        [U,S,V] = svd(frame);
+        frame_double = im2double(frame);
+        [U,S,V] = svd(frame_double);
         disp(frame_num)
 
     end
 
-mov = VideoReader(videofile);
+    mov = VideoReader(videofile);
 
-frame_num = 0;
-while mov.hasFrame
-    frame = mov.readFrame();
-    
-    frame_gray = frame(:,:,1);
-    frame_num = frame_num +1 ;
-    process(frame_gray, frame_num)
+    frame_num = 0;
+    while mov.hasFrame
+        frame = mov.readFrame();
+        
+        frame_gray = frame(:,:,1);
+        frame_num = frame_num +1 ;
+        process(frame_gray, frame_num)
+    end
 end
-
